cc7e448f7cb9c9e436bbd7edb05833d90f8e5065
maria <maria.paula@sptech.school>
2026-03-01 17:33:17 -0300

    chore: config inicial do backEnd do projeto

---
+++ fluxy-api/src/main/java/school/sptech/fluxyapi/FluxyApiApplication.java
@@ -0,0 +1,13 @@
+package school.sptech.fluxyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FluxyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FluxyApiApplication.class, args);
+    }
+
+}
---
+++ fluxy-api/src/test/java/school/sptech/fluxyapi/FluxyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package school.sptech.fluxyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FluxyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
