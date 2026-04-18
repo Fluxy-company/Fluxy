c159a76c4f0f67fe4fcf41ba463c9c5126c38eba
Rebeca <rebeca.ferreira@sptech.school>
2026-04-18 00:33:20 -0300

    inicio do desenvolvimento de componentes do site institucional

---
+++ fluxy-frontend/fluxy-institucional/components/heroSection/HeroSection.jsx
@@ -0,0 +1,7 @@
+export default function HeroSection(){
+  return (
+    <section className="bg-gradient-to-b from-[#0E1116] to-[#22344E]">
+      
+    </section>
+  );
+}
---
+++ fluxy-frontend/fluxy-institucional/src/App.jsx
@@ -0,0 +1,12 @@
+import HeroSection from '../components/heroSection/HeroSection'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <HeroSection/>
+    </>
+  )
+}
+
+export default App
